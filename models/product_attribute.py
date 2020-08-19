c5162cdba4618463a2b47f712205fb3f9d3f4919
Pafnow <pafnow@hotmail.com>
2020-08-19 13:46:27 +0700

    [WIP] refactor: add attribute_type and rollback standard display_type

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -1,32 +1,60 @@
 # Copyright 2020 Pafnow
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductAttribute(models.Model):
     _inherit = "product.attribute"
 
-    display_type = fields.Selection(selection_add=[('input', 'User Input')])
+    #display_type = fields.Selection(selection_add=[('input', 'User Input')])
+    attribute_type = fields.Selection([
+        ('list', 'List'),
+        ('char', 'Char'),
+        ('int', 'Integer'),
+        ('float', 'Float'),
+    ], default='list', required=True, compute='_compute_attribute_type', inverse='_inverse_attribute_type')
 
-    default_value_id = fields.Many2one('product.attribute.value', compute='_compute_default_value_id')
-    default_value_input_type = fields.Selection(related='default_value_id.input_type', readonly=False)
-    default_value_input_min = fields.Float(related='default_value_id.input_min', readonly=False)
-    default_value_input_max = fields.Float(related='default_value_id.input_max', readonly=False)
+    custom_value_id = fields.Many2one('product.attribute.value', compute='_compute_custom_value_id')
+    custom_value_input_min = fields.Char(related='custom_value_id.input_min', readonly=False)
+    custom_value_input_max = fields.Char(related='custom_value_id.input_max', readonly=False)
 
-    @api.depends('display_type')
-    def _compute_default_value_id(self):
+    @api.depends('custom_value_id', 'custom_value_id.input_type')
+    def _compute_attribute_type(self):
         for record in self:
-            if record.display_type != 'input':
-                self.default_value_id = None
+            if record.custom_value_id and record.custom_value_id.input_type:
+                record.attribute_type = record.custom_value_id.input_type
             else:
-                if not len(self.value_ids):
-                    self.value_ids = [(0, 0, {
+                record.attribute_type = 'list'
+
+    @api.onchange('attribute_type')
+    def _inverse_attribute_type(self):
+        for record in self:
+            v_attribute_type = record.attribute_type #Use local variable here because some recomputation might happen below
+
+            if v_attribute_type == 'list':
+                if record.custom_value_id:
+                    record.custom_value_id.input_type = None
+            else:
+                #Create custom value record if no record
+                if not len(record.value_ids):
+                    record.value_ids = [(0, 0, {
                         'name': 'Value',
                         'is_custom': True,
                     })]
-                #//TODO: Handle case with multiple value_ids. Should delete all but the first/default one?
-                #//TODO: Try to fetch the value marked as default, instead of first record [0]
-                self.default_value_id = self.value_ids[0]
+                if not record.custom_value_id:
+                    raise ValidationError("Please delete all attribute values before changing Attribute Type (no custom value found).")
+                record.custom_value_id.input_type = v_attribute_type
+                record.display_type = 'radio'
+                record.create_variant = 'no_variant'
+
+    @api.depends('value_ids')
+    def _compute_custom_value_id(self):
+        for record in self:
+            if len(record.value_ids) == 1 and record.value_ids[0].name == 'Value' and record.value_ids[0].is_custom:
+                record.custom_value_id = record.value_ids[0]
+            else:
+                record.custom_value_id = None
 
 
 class ProductAttributeValue(models.Model):
@@ -37,5 +65,56 @@ class ProductAttributeValue(models.Model):
         ('int', 'Integer'),
         ('float', 'Float'),
     ])
-    input_min = fields.Float("Min Value", digits=(12, 6))
-    input_max = fields.Float("Max Value", digits=(12, 6))
+    input_min = fields.Char("Min Value")
+    input_max = fields.Char("Max Value")
+
+    @api.constrains('input_type', 'input_min')
+    def _check_input_min(self):
+        """ Ensure input_min is of the same type than input_type """
+        for record in self:
+            if record.input_min and record.input_type == 'int':
+                try:
+                    int(record.input_min)
+                except ValueError:
+                    raise ValidationError("Min Value cannot be converted to an integer")
+            if record.input_min and record.input_type == 'float':
+                try:
+                    float(record.input_min)
+                except ValueError:
+                    raise ValidationError("Min Value cannot be converted to a float")
+
+    @api.constrains('input_type', 'input_max')
+    def _check_input_max(self):
+        """ Ensure input_max is of the same type than input_type """
+        for record in self:
+            if record.input_max and record.input_type == 'int':
+                try:
+                    int(record.input_max)
+                except ValueError:
+                    raise ValidationError("Max Value cannot be converted to an integer")
+            if record.input_max and record.input_type == 'float':
+                try:
+                    float(record.input_max)
+                except ValueError:
+                    raise ValidationError("Max Value cannot be converted to a float")
+
+
+class ProductAttributeCustomValue(models.Model):
+    _inherit = "product.attribute.custom.value"
+
+    custom_value_char = fields.Char("Custom Value", compute='_compute_custom_value_char')
+    custom_value_int = fields.Integer("Custom Value", compute='_compute_custom_value_int')
+
+    def _compute_custom_value_char(self):
+        for record in self:
+            if not record.custom_product_template_attribute_value_id.product_attribute_value_id.input_type == 'char':
+                record.custom_value_char = None
+            else:
+                record.custom_value_char = record.custom_value
+
+    def _compute_custom_value_int(self):
+        for record in self:
+            if not record.custom_product_template_attribute_value_id.product_attribute_value_id.input_type == 'int':
+                record.custom_value_int = None
+            else:
+                record.custom_value_int = int(record.custom_value)
